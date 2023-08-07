e9f61208b0c5316da7e397b863b77a88ff18bacf
Carter <6k2hbc@gmail.com>
2023-08-07 21:41:43 +0900

    TPC 강의 project02_A 연습 완료

---
+++ src/Default/Project02_A.java
@@ -0,0 +1,30 @@
+package Default;
+
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class Project02_A {
+    public static void main(String[] args) {
+        // Jsoup API
+        String url ="https://sports.news.naver.com/general/index";
+        Document doc = null;
+        try{
+            doc = Jsoup.connect(url).get();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        Elements element = doc.select("div.home_news");
+        String title = element.select("h2").text().substring(0,4);
+        System.out.println("===============");
+        System.out.println(title);
+        System.out.println("================");
+
+        for(Element el :element.select("li")){
+            System.out.println(el.text());
+        }
+        System.out.println("==============");
+    }
+}
